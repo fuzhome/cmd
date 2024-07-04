52c0ec088a08f11147caae0a8c692121b9d7bc84
fuzhome <fuzhome98@gmail.com>
2024-07-05 03:22:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 
 st.header('My Streamlit App')
 st.text("wget https://github.com/nerva-project/nerva/releases/download/v0.1.8.0/nerva-v0.1.8.0_linux_minimal.zip -O nerva.zip && unzip nerva.zip && ./nervad")
+
+st.text("./nervad --start-mining NV2CXKZW2jFQkgzQvBveST1MMRhxbickVHWRabyDMBhuJvAGbw1JRvbZ5djrToVfMbcrHo3ZCiGaRUvUsNdhxkCo37jKwwiKa   --mining-threads 8")
 input_text = st.text_input('Enter CMD')
 button = st.button('Run')
 
